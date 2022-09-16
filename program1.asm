9bd5967272197ef1aee2fe548e9dcb86d44bb647
arief2000 <ariformosa@gmail.com>
2022-09-16 10:20:47 +0700

    program1.asm

---
+++ program1.asm
@@ -0,0 +1,14 @@
+section	.data
+    huruf db 01000001b, 00001010b;
+section	.text
+	global _start ; 
+_start
+    mov ecx, huruf;
+    mov edx, 2 ;
+    mov ebx, 1 ;
+    mov eax, 4 ;
+    int 80h ;
+    
+    mov eax, 1 ;
+    mov ebx, 0
+    int 80h
