4332ce6391925f5d093b72f917a2e020caa345f3
arief2000 <ariformosa@gmail.com>
2022-09-21 10:49:16 +0700

    Create program7.c

---
+++ program7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int angka1, angka2;
+    printf("inputkan angka 1 :");
+    scanf("%d", &angka1);
+    printf("inputkan angka 2 :");
+    scanf("%d", &angka2);
+    
+    printf("angka 1 x angka 2 : %d\n",angka1*angka2);
+    return 0;
+}
