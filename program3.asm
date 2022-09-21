517bf90642242935e7eeab36001faedfcb9dd482
arief2000 <ariformosa@gmail.com>
2022-09-21 10:46:36 +0700

    Update program3.asm

--- program3.asm
+++ program3.asm
@@ -1,24 +1,41 @@
-BEGIN TRANSACTION
-SET QUOTED_IDENTIFIER ON
-SET ARITHABORT ON
-SET NUMERIC_ROUNDABORT OFF
-SET CONCAT_NULL_YIELDS_NULL ON
-SET ANSI_NULLS ON
-SET ANSI_PADDING ON
-SET ANSI_WARNINGS ON
-COMMIT
-BEGIN TRANSACTION
-GO
-ALTER TABLE dbo.LayoutPemeriksaan ADD
-	Memo text NULL
-GO
-ALTER TABLE dbo.LayoutPemeriksaan SET (LOCK_ESCALATION = TABLE)
-GO
-COMMIT
-select Has_Perms_By_Name(N'dbo.LayoutPemeriksaan', 'Object', 'ALTER') as ALT_Per, Has_Perms_By_Name(N'dbo.LayoutPemeriksaan', 'Object', 'VIEW DEFINITION') as View_def_Per, Has_Perms_By_Name(N'dbo.LayoutPemeriksaan', 'Object', 'CONTROL') as Contr_Per 
-GO
+section .data
+    segment .bss
+        angka1 resb 2
+        angka2 resb 2
+        hasil resb 2
+section .text
+    global _start
+_start:
+    ;block angka1
+    mov ecx, angka1
+    mov edx, 2
+    mov ebx, 2
+    mov eax, 3
+    int 80h
+   ;block angka2
+    mov ecx, angka2
+    mov edx, 2
+    mov ebx, 2
+    mov eax, 3
+    int 80h
+    ;block process
+    mov eax, [angka1]
+    sub eax, '0'
+    mov ebx, [angka2]
+    sub ebx, '0'
 
-SELECT * FROM GrupLayout
+    add eax, ebx
+    add eax, '0'
+    mov [hasil], eax
 
-select * from ruang where JenisRuang='PENUNJANGMEDIS'
+    ;block putput hasil
+    mov ecx, hasil
+    mov edx, 2
+    mov ebx, 1
+    mov eax, 4
+    int 80h
 
+    ;block sysexit
+    mov eax, 1
+    mov ebx, 0
+    int 80h
